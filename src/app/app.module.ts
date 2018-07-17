bed1f510da3e0cc48b31b29a55e977ccfbe10e61
David <davidsg.dev@gmail.com>
2018-07-17 00:58:17 -0300

    add routes and router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,35 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {TransferHttpCacheModule} from '@nguniversal/common';
-import {MatToolbarModule, MatMenuModule, MatButtonModule} from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TransferHttpCacheModule } from '@nguniversal/common';
+import { MatToolbarModule, MatMenuModule, MatButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app.routing.module';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HomeComponent,
-    ],
-    imports: [
-        BrowserModule.withServerTransition({appId: 'gdg-poa'}),
-        RouterModule.forRoot([
-            {path: '', component: HomeComponent, pathMatch: 'full'},
-            {path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
-            {path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
-        ]),
-        TransferHttpCacheModule,
-        MatToolbarModule,
-        MatMenuModule,
-        BrowserAnimationsModule,
-        MatButtonModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ScheduleComponent,
+    ContactComponent,
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'gdg-poa' }),
+    TransferHttpCacheModule,
+    MatToolbarModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
