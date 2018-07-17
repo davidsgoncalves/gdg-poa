d9df1ed2227cf84836a078020327da404a1f5e45
David <davidsg.dev@gmail.com>
2018-07-17 00:20:08 -0300

    add material ui to project ˆ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,32 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
 import {TransferHttpCacheModule} from '@nguniversal/common';
+import {MatToolbarModule, MatMenuModule, MatButtonModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-  ],
-  imports: [
-    BrowserModule.withServerTransition({appId: 'gdg-poa'}),
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full'},
-      { path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
-      { path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
-    ]),
-    TransferHttpCacheModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+    ],
+    imports: [
+        BrowserModule.withServerTransition({appId: 'gdg-poa'}),
+        RouterModule.forRoot([
+            {path: '', component: HomeComponent, pathMatch: 'full'},
+            {path: 'lazy', loadChildren: './lazy/lazy.module#LazyModule'},
+            {path: 'lazy/nested', loadChildren: './lazy/lazy.module#LazyModule'}
+        ]),
+        TransferHttpCacheModule,
+        MatToolbarModule,
+        MatMenuModule,
+        BrowserAnimationsModule,
+        MatButtonModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
