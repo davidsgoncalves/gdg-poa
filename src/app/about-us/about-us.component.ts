de827eb7c77164e1e6f5980514e84a9d3708268f
David <davidsg.dev@gmail.com>
2018-07-18 19:33:49 -0300

    add routes and router module

---
+++ src/app/about-us/about-us.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  template: `
+    <p>
+      about-us works!
+    </p>
+  `,
+  styles: []
+})
+export class AboutUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
